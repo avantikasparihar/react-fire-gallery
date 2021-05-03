908675a7d63ca56ea21d83229ccf209ab8adb36a
Avantika Singh Parihar <avantikasparihar@gmail.com>
2021-05-03 15:03:56 +0530

    missing alt tags added

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -4,8 +4,9 @@ const Title = () => {
   return (
     <div className="title">
       <div className="top">
-        <img src="https://img.icons8.com/plasticine/48/000000/react.png"/>
-        <img src="https://img.icons8.com/color/48/000000/firebase.png" /><h1 className="pt-10" >Gallery</h1>
+        <img src="https://img.icons8.com/plasticine/48/000000/react.png" alt="react logo" />
+        <img src="https://img.icons8.com/color/48/000000/firebase.png" alt="firebase logo" />
+        <h1 className="pt-10" >Gallery</h1>
       </div>
       <h2>Your Pictures</h2>
       <p>Click button to add more.</p>
